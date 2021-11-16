f7893f6403c8c08653ccee8a7c185814ece4dcbe
andersonjmv <anderson.manosalva@gmail.com>
2021-11-16 11:47:42 -0500

    add

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,36 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="px-4 py-3 my-5 text-center">
+    <img
+      class="d-block mx-auto mb-4"
+      src="https://cdn.worldvectorlogo.com/logos/vue-js-1.svg"
+      alt=""
+      width="120"
+      height="87"
+    />
+    <h1 class="display-5 fw-bold">VueJS 3 Crud + Mysql</h1>
+    <div class="col-lg-6 mx-auto">
+      <h2>Crud</h2>
+      <ul class="list-group">
+        <li class="list-group-item">A simple default list group item</li>
+        <li class="list-group-item list-group-item-secondary">
+          Ver
+        </li>
+        <li class="list-group-item list-group-item-success">
+         Crear
+        </li>
+        <li class="list-group-item list-group-item-danger">
+         Borrar
+        </li>
+        <li class="list-group-item list-group-item-info">
+          Editar
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+export default {};
 </script>
